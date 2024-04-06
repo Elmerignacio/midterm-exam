f582001b8b062c4f4f49b00f8f722168c081690e
RoyRabuyo <royrabuyo555@gmail.com>
2024-04-06 23:28:55 +0800

    elmer

---
+++ Home/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/CSS/Home.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+</head> 
+<body>
+   
+      <div class="title">
+        <p class="para1">Student Job Network</p>
+        </div>
+    <div id="navigation">
+  <div class="inline">
+    <div id="menu" onclick="onClickMenu()">
+      <div id="bar1" class="bar"></div>	
+      <div id="bar2" class="bar"></div>
+      <div id="bar3" class="bar"></div>
+    </div>
+    <ul class="nav" id="nav">
+      <li><a href="/Home/index.php"><i home class="fa-solid fa-house"></i></a></li>
+      <li><a href="/search-job/search.php"><i class="fa-solid fa-briefcase"></i></a></li>
+      <li><a href="/Save-jobs/index.php"><i class="fa-solid fa-bookmark"></i></a></li>
+      <li><a href="/Application/index.php"><i class="fa-solid fa-note-sticky"></i></a></li>
+      <li><a href="/user/index.php"><i class="fa-solid fa-user"></i></a></li>
+      <li><a href="/index.php"><i class="fa-solid fa-right-from-bracket"></i></a></li>
+    </ul>
+  </div>
+</div>
+  </div>
+  <div class="block">
+    <p class="para">Featured Companies
+  </p></div>
+ 
+ <div class="slider">
+  <div class="arrow prevBtn">&lt;</div>
+  <div class="arrow nextBtn">&gt;</div>
+
+<div class="margin">
+<div class="div1">
+  <div class="box1">
+  <div><img src="/images/1.png" >
+  <p class="p1">StudentWork Solution</p>  
+  <a href="/Student-Solution/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/2.png">
+  <p class="p2">Academic Allies Staffing</p>  
+  <a href="/Academic-Allies-Staffing/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/3.png">
+  <p class="p3">College Co-op Careers</p>  
+  <a href="/College-Co-op-Careers/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/4.png">
+  <p class="p4">Scholarly Side Jobs Inc.</p>  
+  <a href="/Scholarly-Side-Jobs-Inc/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/5.png">
+  <p class="p5">EduFlex Employment Agency</p>  
+  <a href="/EduFlex-Employment-Agency/details.php">Go to Details</a>
+  </div>
+</div>
+</div> <br>
+
+<div class="div1">
+  <div class="box1">
+  <div><img src="/images/6.png">
+  <p class="p6">Campus Hustle Hub</p>  
+  <a href="/Campus-Hustle-Hub/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/7.png">
+  <p class="p7">Study Break Jobs</p>  
+  <a href="/Study-Break-Jobs/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/8.png">
+  <p class="p8">Scholarly Services</p>  
+  <a href="/Scholarly-Services/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/9.png">
+  <p class="p9">Study & Earn Inc</p>  
+  <a href="/Study-&-Earn-Inc/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/10.png">
+  <p class="p10">FlexiWork Solutions</p>  
+  <a href="/FlexiWork-Solutions/details.php">Go to Details</a>
+  </div>
+</div>
+</div><br>
+
+<div class="div1">
+  <div class="box1">
+  <div><img src="/images/11.png">
+  <p class="p11">College Classmate Careers</p>  
+  <a href="/College-Classmate-Careers/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/12.png">
+  <p class="p12">Study Support Squad</p>  
+  <a href="/Study-Support-Squad/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/13.png">
+  <p class="p13">StudyBuddy Solutions</p>  
+  <a href="/StudyBuddy-Solutions/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/14.png">
+  <p class="p14">Classmate Career Consortium</p>  
+  <a href="/Classmate-Career-Consortium/details.php">Go to Details</a>
+  </div>
+</div>
+</div>
+
+<div class="div1">
+  <div class="box2">
+  <div><img src="/images/15.png">
+  <p class="p15">Academic Allies Connectors</p>  
+  <a href="/Academic-Allies-Connectors/details.php">Go to Details</a>
+  </div>
+</div>
+</div><br>
+</div>
+
+</body>
+</html>
+<script src="/Home/script.js"></script>
